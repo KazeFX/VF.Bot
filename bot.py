c249f9003cfa3906e14c4723e06b36b343a48194
KazeFX <1071525+KazeFX@users.noreply.github.com>
2026-07-21 20:59:33 +0200

    Bot trap section added.

--- bot.py
+++ bot.py
@@ -1,7 +1,33 @@
 import discord
-from discord.ext import commands, tasks
 import requests
-from api_keys import BOT_TOKEN
+import database_helper
+from discord.ext import commands, tasks
+from api_keys import BOT_TOKEN, BOT_TRAP_CHANNEL_ID
+
+intents = discord.Intents.default()
+intents.members = True # Required to receive member join events
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all(), help_command=None)
+
+
+@bot.event
+async def on_ready():
+    print("VF.Bot logged in!")
+
+
+@bot.event
+async def on_message(message):
+    # Bot trap section
+    if message.author.bot:
+        return
+    if message.channel.id == BOT_TRAP_CHANNEL_ID:
+        try:
+            await message.author.ban(reason="Posted in restricted channel, probably a bot.")
+            await message.channel.send(f"{message.author} was probably a bot, banned!")
+        except Exception as e:
+            print(f"Failed to ban user: {e}")
+
+    await bot.process_commands(message)
 
 
 
+bot.run(BOT_TOKEN)
